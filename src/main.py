3886d09a12ef81963a06b1816e4b1f0ceabef3bf
Pablo Hernández Carrascosa <pablohc224@gmail.com>
2021-12-03 12:33:41 +0100

    Add some preprocessing (remove columns, rows with NA, parse columns and onehotencoding)

--- src/main.py
+++ src/main.py
@@ -3,20 +3,4 @@ from preprocessing import *
 
 spark = SparkSession.builder.appName('Practise').getOrCreate()
 
-load_data(spark)
-
-'''
-df.show()
-print(df.count())
-df.groupby('Year').avg().show()
-df.groupby('Month').avg().show()
-df.groupby('DayofMonth').avg().show()
-df.groupby('DayOfWeek').avg().show()
-'''
-
-'''
-for file in os.listdir():
-    print(file)
-    df_pyspark = spark.read.csv('../data/'+file, header=True)
-    df_pyspark.show()
-'''
+df = load_data(spark)
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,34 @@
+from pyspark.ml.feature import OneHotEncoder
+
+
 def load_data(spark):
-    df = spark.read.csv("../data/2008.csv", header=True)
-    df.show()
-    print(df.columns)
+    df = spark.read.csv("../data/2008.csv", header=True, inferSchema=True)
+    # Remove forbidden variables
     df = df.drop(*('ArrTime', 'ActualElapsedTime', 'AirTime', 'TaxiIn', 'Diverted', 'CarrierDelay', 'WeatherDelay',
                    'NASDelay', 'SecurityDelay', 'LateAircraftDelay'))
-    print(df.columns)
+    # Remove useless variables
+    df = df.drop('CancellationCode')
 
+    print(df.count())
+    # Remove canceled flights
+    df = df.replace('NA', None)
+    df = df.na.drop(how='any', subset=['ArrDelay'])
+    print(df.count())
+
+    # Remove rows with NA values
+    df = df.na.drop(how='any')
+    print(df.count())
+
+    # Parse columns from string to integer or double
+    df = df.withColumn('DepTime', df['DepTime'].cast('integer'))
+    df = df.withColumn('CRSElapsedTime', df['CRSElapsedTime'].cast('integer'))
+    df = df.withColumn('ArrDelay', df['ArrDelay'].cast('double'))
+    df = df.withColumn('DepDelay', df['DepDelay'].cast('integer'))
+    df = df.withColumn('TaxiOut', df['TaxiOut'].cast('integer'))
+
+    encoder = OneHotEncoder(inputCols=["UniqueCarrier", "TailNum", "Origin", "Dest"],
+                            outputCols=["UniqueCarrier", "TailNum", "Origin", "Dest"])
+    model = encoder.fit(df)
+    df = model.transform(df)
+
+    return df
