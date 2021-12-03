95447611ca9407f27ccea707fdf24f8253cb7b9e
JuanSantacruzzz <santacruzzzjuan@gmail.com>
2021-12-03 15:36:00 +0100

    one hot encoding

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,13 +1,13 @@
-from pyspark.ml.feature import OneHotEncoder
+from pyspark.ml.feature import OneHotEncoder, StandardScaler, StringIndexer
 
 
 def load_data(spark):
-    df = spark.read.csv("../data/2008.csv", header=True, inferSchema=True)
+    df = spark.read.csv('../data/2008.csv', header=True, inferSchema=True)
     # Remove forbidden variables
     df = df.drop(*('ArrTime', 'ActualElapsedTime', 'AirTime', 'TaxiIn', 'Diverted', 'CarrierDelay', 'WeatherDelay',
                    'NASDelay', 'SecurityDelay', 'LateAircraftDelay'))
     # Remove useless variables
-    df = df.drop('CancellationCode')
+    df = df.drop(*('CancellationCode', 'TailNum'))
 
     print(df.count())
     # Remove canceled flights
@@ -26,9 +26,17 @@ def load_data(spark):
     df = df.withColumn('DepDelay', df['DepDelay'].cast('integer'))
     df = df.withColumn('TaxiOut', df['TaxiOut'].cast('integer'))
 
-    encoder = OneHotEncoder(inputCols=["UniqueCarrier", "TailNum", "Origin", "Dest"],
-                            outputCols=["UniqueCarrier", "TailNum", "Origin", "Dest"])
-    model = encoder.fit(df)
-    df = model.transform(df)
+    # OneHotEncoder
+    indexer = StringIndexer(inputCols=['UniqueCarrier', 'Origin', 'Dest'],
+                            outputCols=['UniqueCarrier_index', 'Origin_index', 'Dest_index']).fit(df)
+    df = indexer.transform(df)
+    indexer = OneHotEncoder(inputCols=['UniqueCarrier_index', 'Origin_index', 'Dest_index'],
+                            outputCols=['UniqueCarrier_ohe', 'Origin_index_ohe', 'Dest_index_ohe']).fit(df)
+    df = indexer.transform(df)
+    df = df.drop(*('Dest', 'Origin', 'UniqueCarrier', 'Dest_index', 'Origin_index', 'UniqueCarrier_index'))
+    df = df.withColumnRenamed('UniqueCarrier_ohe', 'UniqueCarrier') \
+        .withColumnRenamed('Origin_index_ohe', 'Origin') \
+        .withColumnRenamed('Dest_index_ohe', 'Dest')
+    df.show()
 
     return df
