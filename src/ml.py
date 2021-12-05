beba0ffeed4dbf1552c339a4c1dad0fdf386fcaa
Pablo Hernández Carrascosa <pablohc224@gmail.com>
2021-12-05 11:18:45 +0100

    Refactor ml files vy removing unused methods

--- src/ml.py
+++ src/ml.py
@@ -1,27 +1,8 @@
-from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
-
-
-def prepare_df(df, label):
-    columns = df.columns
-    columns.remove(label)
-
-    predictors = VectorAssembler(inputCols=columns, outputCol='features')
-    df = predictors.transform(df)
-    df = df.withColumnRenamed(label, 'label')
-    df = df.drop(*columns)
-    return df
 
 
 def split_data(df, trainRatio=0.8):
     return df.randomSplit([trainRatio, 1 - trainRatio], seed=12345)
-
-
-def linear_regression(train, test):
-    lr = LinearRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8, featuresCol='predictors', labelCol='target')
-    lrModel = lr.fit(train)
-    print("Coefficients: %s" % str(lrModel.coefficients))
-    print("Intercept: %s" % str(lrModel.intercept))
 
 
 def linear_regression_model(train):
